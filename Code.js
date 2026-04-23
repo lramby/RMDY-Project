e8f928e21b7c6fec6d23627ea2f07bfdeda74ce0
Lloyd Ramby <lramby@gmail.com>
2026-04-23 17:15:49 -0500

    fix tasks

--- Code.js
+++ Code.js
@@ -74,12 +74,11 @@ function getEquipmentData() {
     return {
       rowIndex: index + 2, // 1 for header, 1 for 0-indexing
       pid: row[cols.PID],
+			roomID: row[cols.ROOMID],
+			taskID: row[cols.TASKID],
       itemID: row[cols.ITEMID],
-      roomID: row[cols.ROOMID],
-      taskID: row[cols.TASKID],
       roomName: row[cols.ROOMNAME],
       taskName: row[cols.TASKNAME],
-      item: row[cols.ITEM],
       value: row[cols.VALUE],
       unit: row[cols.UNIT],
       price: row[cols.PRICE],
